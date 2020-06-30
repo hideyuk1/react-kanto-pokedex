ac03d218f0501ede861d63393a1c4f21b5694142
Lucas China <lucashideyuki@gmail.com>
2020-06-29 23:32:32 -0300

    Create PokemoSprite component

---
+++ src/components/PokemonSprite/PokemonSprite.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './PokemonSprite.module.css';
+import pokedexJson from 'assets/data/pokedex.json';
+
+function PokemonSprite({ pokedexNumber = null, className, ...otherProps }, ref) {
+    if (pokedexNumber === null) return null;
+    const src = pokedexNumber === null ? '' : require(`assets/sprites/${pokedexNumber}.png`);
+    return (
+        <img
+            className={`${styles.sprite} ${className}`}
+            ref={ref}
+            src={src}
+            alt={pokedexJson[pokedexNumber - 1].name}
+            {...otherProps}
+        />
+    );
+}
+
+export default React.forwardRef(PokemonSprite);
