a303a5a526653c1bf0108f4faa0da506a527a384
Lucas China <lucashideyuki@gmail.com>
2020-06-29 23:38:54 -0300

    Update App.js with the created pokedex components and add link to github repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from 'App.module.css';
+import FloatingSearch from 'components/FloatingSearch.js/FloatingSearch';
+import PokemonGrid from 'components/PokemonGrid/PokemonGrid';
+import 'global.module.css';
+import React, { useCallback, useState } from 'react';
+import { FiGithub } from 'react-icons/fi';
+import 'typeface-spartan';
+import 'web-animations-js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedPokemonId, setSelectedPokemonId] = useState(null);
+    const [searchInput, setSearchInput] = useState('');
+
+    const filterFunction = useCallback(
+        (pokedexEntry) => pokedexEntry.name.toLowerCase().includes(searchInput.toLowerCase()),
+        [searchInput],
+    );
+
+    return (
+        <main className={styles.container}>
+            <h1 className={styles.title}>Kanto Pokedex</h1>
+            <a
+                className={styles.githubLink}
+                target="_blank"
+                href="https://github.com/hideyuk1/react-kanto-pokedex"
+                rel="noopener noreferrer"
+            >
+                <FiGithub />
+            </a>
+            <PokemonGrid
+                selectedPokemonId={selectedPokemonId}
+                setSelectedPokemonId={setSelectedPokemonId}
+                filterFunction={filterFunction}
+            />
+            <FloatingSearch value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+        </main>
+    );
 }
 
 export default App;
